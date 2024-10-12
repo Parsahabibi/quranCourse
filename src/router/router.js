d23f695203a2a310c3cc4cf6e6bf0b9d43672085
parsahabibi <parsahabibi@parsahabibis-MacBook-Air.local>
2024-10-12 23:50:13 +0330

    fix invalid routing

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
-import Airplane from "../Components/AirplaneMenu/Airplane";
-import ScrollMenu from '../Components/DummyMenu/ScrollMenu';
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Home from '../pages/Home'
 import DummyMenu from "../pages/DummyMenu";
 
@@ -17,5 +15,9 @@ const router = createBrowserRouter([
         path: "/Menu",
         element: <DummyMenu />,
     },
+    {
+        path: "*",  // This catches any undefined route
+        element: <Navigate to="/" />,  // Redirect to Home
+    }
 ])
 export default router
